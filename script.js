7dcadacd9f4dd3efc9fa8f72623bfac87277efcd
Lucas Santos Da Silva <84152031+Lucas-Santos-Da-Silva@users.noreply.github.com>
2024-07-01 16:00:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then((registration) => {
+            console.log('Service Worker registrado com escopo: ', registration.scope)
+        }).catch((error) => {
+            console.log('Falha no registro do Service Worker: ', error)
+        })
+}
+
+
 const gridContainer = document.querySelector(".grid-container");
 let cards = [];
 let  firstCard, secondCard;
