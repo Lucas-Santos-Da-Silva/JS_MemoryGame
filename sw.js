8c0e48d751fc059b54c8ea68d4bb364e898c5d43
Lucas Santos Da Silva <84152031+Lucas-Santos-Da-Silva@users.noreply.github.com>
2024-07-24 00:15:56 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-/*
+
 // Nome do cache (controle de versão)
 const cachePWA = 'cache-v2'
 const files_to_cache = [
@@ -65,66 +65,5 @@ async function registerPeriodicNewsCheck() {
     console.log("Periodic Sync could not be registered!");
   }
 }
-*/
 
 
-// Nome do cache (controle de versão)
-const cachePWA = 'cache-v2'
-
-// Arquivos a serem armazenados em cache
-// Todos os arquivos devem ser adicionados ao vetor (exceto o manifesto)
-
-// Instalando o Service Worker e armazenando os arquivos no cache
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(cachePWA)
-      .then((cache) => {
-        return cache.addAll([
-          '/index.html',  
-          '/style.css',
-          '/script.js',
-          '/sw.js',
-          '/manifest.json',
-          '/data/cards.json',
-          '/assets/cherries.png',
-          '/assets/chili.png',
-          '/assets/grapes.png',
-          '/assets/icon.png',
-          '/assets/lemon.png',
-          '/assets/orange.png',
-          '/assets/pineapple.png',
-          '/assets/strawbery.png',
-          '/assets/tomato.png',
-          '/assets/watermelon.png'    
-        ])
-      })
-  )
-})
-
-// Interceptando as solicitações de rede e servindo arquivos do cache quando offline
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Se o arquivo está no cache, serve o arquivo do cache
-        if (response) {
-          return response
-        }
-        // Caso contrário, faz uma solicitação de rede
-        return fetch(event.request)
-      })
-  )
-})
-
-// O Periodic Sync permite que as aplicações web alertem seu Service Worker para fazer atualizações em um intervalo de tempo periodico
-// Neste caso a atualização ocorre no periodo de 1 dia
-async function registerPeriodicNewsCheck() {
-  const registration = await navigator.serviceWorker.ready;
-  try {
-    await registration.periodicSync.register("get-latest-news", {
-      minInterval: 1 * 1 * 1 * 1000,
-    });
-  } catch {
-    console.log("Periodic Sync could not be registered!");
-  }
-}
